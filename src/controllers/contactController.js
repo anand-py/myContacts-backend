29dd64cc389c781cf4ee23f11e033606edceb388
Anand <aanand.py@gmail.com>
2023-12-26 20:15:04 +0530

    create contactSchema and connected with db

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -1,44 +1,48 @@
+const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel")
+
 //@desc Get all contacts
 //@route GET/api/contacts
 //@access public
 
-const getContacts = (req,res)=>{
-    return res.status(200).json(req.params.id)
-}
+const getContacts = asyncHandler(async (req,res)=>{
+    const contacts = await Contact.find();
+    return res.status(200).json(contacts)
+});
 //@desc create contacts
 //@route POST/api/contacts
 //@access public
-const createContact = (req,res)=>{
-    console.log("req body:", req.body)
+const createContact = asyncHandler(async (req,res)=>{
+    console.log("The req body is :", req.body)
     const {name, email, phone} = req.body;
-    if(!name || email || phone){
+    if(!name || !email || !phone){
         res.status(400);
         throw new Error("All Fields are mandatory")
     }
-    res.status(201).json({message : "Create Contacts"})
-}
+    res.status(201).json({message : "Create Contact!"})
+});
 
 //@desc Get  contact by id
 //@route GET/api/contacts:id
 //@access public
 
-const getContact = (req,res)=>{
+const getContact = asyncHandler( (req,res)=>{
     res.status(200).json({message : `Get contact for ${req.params.id}`})
-}
+});
 
 //@desc update contacts
 //@route PUT/api/contacts/:id
 //@access public
-const updateContact = (req,res)=>{
+const updateContact = asyncHandler(async (req,res)=>{
     res.status(200).json({message : `update contact for ${req.params.id}`})
-}
+});
 
 //@desc delete contact
 //@route delete/api/contacts/:id
 //@access public
-const deleteContact = (req,res)=>{
+const deleteContact =  asyncHandler(async (req,res)=>{
     res.status(200).json({message : `Delete contact for ${req.params.id}`})
-}
+});
 
 
 
--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,5 +1,46 @@
+const { constants } = require("express-async-handler")
 const errorHandler = (err,req,res,next)=>{
+    const statusCode = res.statusCode ? res.statusCode : 500;
+    switch (statusCode) {
+        case constants.VALIDATION_ERROR:
+            res.json({
+                title : "Validation Failed", 
+                message : err.message, 
+                stackTrace : err.stack
+            });
+    
+        case constants.NOT_FOUND:
+                res.json({
+                    title : "Not Found", 
+                    message : err.message, 
+                    stackTrace : err.stack
+            });
 
+        case constants.UNAUTHORIZED:
+                res.json({
+                    title : "Unauthorized", 
+                    message : err.message, 
+                    stackTrace : err.stack
+            });
+
+        case constants.FORBIDDEN:
+            res.json({
+                title : "Forbidden", 
+                message : err.message, 
+                stackTrace : err.stack
+            });
+
+        case constants.SERVER_ERROR:
+            res.json({
+                title : "Server Error", 
+                message : err.message, 
+                stackTrace : err.stack
+            });
+        default:
+            console.log("No Error Found")
+            break;
+    }
+    
 }
 
-module.exports = errorHandler
+module.exports = errorHandler;
